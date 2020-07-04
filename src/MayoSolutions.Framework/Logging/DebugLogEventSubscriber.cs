0443298a432ff0ccaa124148c941dbad3398a5e5
rmayo298 <Robert_Mayo2@comcast.com>
2020-07-04 19:03:36 -0400

    Added DebugLogEventSubscriber

---
+++ src/MayoSolutions.Framework/Logging/DebugLogEventSubscriber.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MayoSolutions.Framework.Logging
+{
+    public class DebugLogEventSubscriber : ILogEventSubscriber
+    {
+        public virtual Task Receive(LogEntry entry)
+        {
+            Debug.WriteLine(entry.Message);
+            return Task.CompletedTask;
+        }
+
+    }
+}
